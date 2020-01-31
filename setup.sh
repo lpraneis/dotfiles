187273377f959ff5b818268ee89c3af668b212b7
Logan Praneis <lpraneis@gmail.com>
2020-01-30 20:09:10 -0600

    Fixed coc-settings

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 echo "source ~/dotfiles/.zshrc" > $HOME/.zshrc
 echo "source ~/dotfiles/init.vim" > $HOME/.config/nvim/init.vim
-cp ./coc-settings.json $HOME/./config/nvim/coc-settitngs.json
+cp ./coc-settings.json $HOME/.config/nvim/coc-settitngs.json
 
