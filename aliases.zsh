8baa75f08a9c9d52e8c4dc1de746459071e16a75
Logan Praneis <lpraneis@gmail.com>
2020-04-04 18:48:52 -0500

    Added IP alias

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,6 @@ alias vi="nvim"
 alias normps="$HOME/bin/normps.sh"
 alias prog="git log -- ."
 alias ghidra="/home/logan/apps/ghidra_9.1.2_PUBLIC/ghidraRun"
-alias publicip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias sage="/home/logan/apps/SageMath/sage"
 alias cat="bat"
 alias ls="ls --color=tty"
@@ -10,9 +9,4 @@ alias umnvpn="/opt/cisco/anyconnect/bin/vpnui"
 alias dc="sudo docker-compose"
 alias loadalgo="sudo /home/logan/Documents/tech/vpn/load-algo.sh"
 alias unloadalgo="sudo /home/logan/Documents/tech/vpn/unload-algo.sh"
-
-cs() { dir=$(find /home/logan/currTerm/ -maxdepth 3 -type d| fzf +m) && cd "$dir"}
-py() { env=$(find /home/logan/python-virtual-environments -maxdepth 1 -type d | fzf +m) &&
-    source "$env/bin/activate"}
-
-mkcd() { mkdir "$1"; cd "$1"; }
+alias publicip="$HOME/bin/publicip.sh"
