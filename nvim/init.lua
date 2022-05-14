da71d331b6e2235cce7ef439f455af51ec39d2e2
Logan Praneis <lpraneis@gmail.com>
2022-05-13 21:23:17 -0500

    Made custom tree-sitter log highlighter depend on github

--- nvim/init.lua
+++ nvim/init.lua
@@ -318,14 +318,14 @@ require('nvim-treesitter.configs').setup {
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.log = {
   install_info = {
-    url = "~/tools/tree-sitter-tracing-log", -- local path or git repo
+    url = "https://github.com/lpraneis/tree-sitter-tracing-log",
     files = {"src/parser.c"},
     -- optional entries:
-    branch = "master", -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    branch = "main",
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
   },
-  filetype = "log", -- if filetype does not match the parser name
+  filetype = "log",
 }
 
 -- hlargs - Highlight function args using treesitter
