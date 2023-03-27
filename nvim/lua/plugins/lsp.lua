92690dc4c65105158d655043471da46237177a71
Logan Praneis <lpraneis@gmail.com>
2023-03-27 09:32:03 -0500

    make lsp lazy

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
 	{ 'lukas-reineke/lsp-format.nvim',       config = true },
 	{
 		'neovim/nvim-lspconfig',
+		lazy = true,
 		dependencies = {
 			-- mason helps auto-install unimportant LSP
 			{ "williamboman/mason.nvim",           config = true },
